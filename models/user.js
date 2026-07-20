cd6a0796b5d0c173b9e5069f296fd894a360e1a5
Bunty sharma <bunty.sharma2642001@gmail.com>
2026-07-21 01:10:18 +0530

    Refactor database connection

--- models/user.js
+++ models/user.js
@@ -1,22 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/team_task_manager",)       
-
-.then(() => {
-  console.log("Connected");
-})
-.catch((err) => {
-  console.log(err);
-});
-
-
-const schema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
 });
 
-
-const User = mongoose.model('User', schema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
